5e6f39ceb66856b7bc4739c70a8c4bb37b9754f5
T4T4K4 <36934833+T4T4K4@users.noreply.github.com>
2022-12-02 17:25:43 +0100

    Solution for day 1 part 1

---
+++ day01_01.py
@@ -0,0 +1,22 @@
+file = open('day01_source.txt', mode = 'r', encoding = 'utf-8-sig')
+lines = file.readlines()
+file.close()
+
+source = []
+set_ = []
+for line in lines:
+    if line !="\n":
+        temp = line.strip("\n")
+        set_.append(int(temp))
+    else:
+        source.append(set_)
+        set_ = []
+
+result = []
+for i, set_ in enumerate(source):
+    suma = sum(set_)
+    result.append([i,suma])
+
+result.sort(key=lambda x: x[1])
+
+print("Result of part 1:", result[-1][1])
